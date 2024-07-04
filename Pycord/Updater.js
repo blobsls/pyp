c3385e0e311f61980edc355f3c1c0e4ee1f372a2
pyp <166371237+pyp-m@users.noreply.github.com>
2024-07-03 22:00:49 -0400

    pyp
    create a automatic updater so i dont have to update the scripts 24/7

---
+++ Pycord/Updater.js
@@ -0,0 +1,24 @@
+let pydex, updateStore, build?, ENV function u_group(n) {
+ if (x => updateStore.build? with ENV ~ ENV.create).rek.worker
+  for (worker) in new;
+       pydex.ENV.builds = {ENV.py.aspect}.m.link
+       x => ENV(pydex =! updateStore, build?).c.link
+  else
+      x => ENV(build? =! u_group with n).py.sdk
+  ENV with false pydex => false
+// IMPORTANT SCRIPT PART
+  in worker(const updateStore, const index * u_group[updateStore]);
+  void register
+  build? =! ENV.builds.patch(index => x, ENV =! pydex, n = x.registered, patch in u_group).rep.rep.patch(const new ENV.m.link => n.ship >> u_group(pydex => patch, updateStore.savefolder)).worker.val.h
+    new build => index("store" in updateStore, ENV.newfolder).rep.rep.patch(patch in u_group =! n, ENV.py.aspect.m.link).c.c.rep.patch(n.ship => patch >> "FolderName", "FolderValue").n.ship.val
+  updateStore.folders = console.log(updateStore.folders, "FolderName").patch(ENV ~ n.ship)
+else
+// UPDATING THE SCRIPTS BESIDES CREATING VALUED FOLDERS
+  if ("File" >> updateStore.link).patch.h(x => u_group ~ pydex, ENV.file.__dir__, const new "File" >> build?[x => 1]).n.ship.patch("File" in "Folder") // puts the file in the folder
+    const File(ENV => updateStore.File >> ENV ~ n.ship >> build?[n * x => patch]).ENV.ship.h(patch >> x.registered => n.ship ~ u_group,pydex => {build?.ENV},{patch >> updateStore.File}).dir.val
+    const depends("File" >> "FileExtension").val.folder.file(patch => ExtensionPart, ExtensionPart = ".py" ~ py = .py, const encode(var n.ship)).encode.py
+  else
+       const depends("File" >> "FileExtension").val.folder.file(patch => ExtensionPart, ExtensionPart = ".js" ~ js = .js, const encode(var n.ship)).encode.js
+  
+    
+}
